ee404339cf00ba97a185c10c9979691250e4657e
LindaOuer <linda.ouerfelli@gmail.com>
2024-02-08 12:02:03 +0100

    S3: Class component - Functional component_ useState + useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import CounterC from "./components/CounterC";
+import CounterF from "./components/CounterF";
 
-import Header from './components/Header'
+import Header from "./components/Header";
 
 function App() {
-  let students = [
-    {id: "1", name: 'Abdelaziz', age: "15"},
-    {id: "2",  name: 'Baha', age: "7" },
-    {id: "3", name: 'Ahemd', age: "80"},
-    
-  ]
-  const style1 = {
-    color: "green",
-    backgroundColor: "red",
-    fontSize: "2rem"
-  }
+    let students = [
+        { id: "1", name: "Abdelaziz", age: "15" },
+        { id: "2", name: "Baha", age: "7" },
+        { id: "3", name: "Ahemd", age: "80" },
+    ];
+    const style1 = {
+        color: "green",
+        backgroundColor: "red",
+        fontSize: "2rem",
+    };
 
-  const handleClick = () => {
-    alert("this is an alert")
-  }
+    const handleClick = () => {
+        alert("this is an alert");
+    };
 
-  let test = true
-  return (
-    <>
-      <Header name = "Header" />
+  let test = true;
+  const [show, setShow] = useState(true)
+  let name = "TWIN7"
+    return (
+        <>
+        <CounterC propsName={test} />
+        <button onClick={() => setShow(!show)}>Show</button>
+        {show && <CounterF name={name} />}
+            {/* <Header name = "Header" />
       <h1 style={style1}>Students List</h1>
       <ul>
       {
@@ -42,9 +49,9 @@ function App() {
 
       {
         test && <p>Test is also True</p>
-      }
-      </>
-  )
+      } */}
+        </>
+    );
 }
 
-export default App
+export default App;
